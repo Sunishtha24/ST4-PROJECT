1bca03278c6e82c0bd96970cfe630e7769acd47b
Sunishtha24 <sunishtha1739.be20@chitkara.edu.in>
2022-10-14 12:57:24 +0530

    st4

---
+++ src/components/users/User.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+
+const User = () => {
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+    phone: "",
+    webiste: ""
+  });
+  const { id } = useParams();
+  useEffect(() => {
+    loadUser();
+  }, []);
+  const loadUser = async () => {
+    const res = await axios.get(`http://localhost:3003/users/${id}`);
+    setUser(res.data);
+  };
+  return (
+
+      <div className="container py-4" style={{color:'#96ada7'}}>
+      <Link className="btn btn-info" to="/">
+        back to Home
+      </Link>
+      <h1 className="display-4" style={{color:'wheat'}}>User Id: {id}</h1>
+      <hr />
+      <ul className="list-group w-80">
+        <li className="list-group-item list-group-item-secondary">name: {user.name}</li>
+        <li className="list-group-item list-group-item-dark">user name: {user.username}</li>
+        <li className="list-group-item list-group-item-secondary">email: {user.email}</li>
+        <li className="list-group-item list-group-item-dark">phone: {user.phone}</li>
+        <li className="list-group-item list-group-item-secondary">website: {user.website}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default User;
